e90b554535f3aa07b229037e22f096e53f34cefa
Benoît Hubert <benoithubert@gmail.com>
2019-11-29 10:53:37 +0100

    filter on department

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const port = 3000;
 
 // respond to requests on `/api/employees`
 app.get('/api/employees', (req, res) => {
+  let selectQuery = 'SELECT * FROM employee';
+  const queryParams = [];
+  if (req.query.department) {
+    selectQuery += ' WHERE department = ?';
+    queryParams.push(req.query.department);
+  }
   // send an SQL query to get all employees
-  connection.query('SELECT * FROM employee', (err, results) => {
+  connection.query(selectQuery, queryParams, (err, results) => {
     if (err) {
       // If an error has occurred, then the client is informed of the error
       res.status(500).send(`An error occurred: ${err.message}`);
